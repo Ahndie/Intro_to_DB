eb9fe8fedb093c3247407f5e06430da6db583691
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-10-20 13:07:47 +0100

    update

--- MySQLServer.py
+++ MySQLServer.py
@@ -25,5 +25,13 @@ def main():
         cursor.close()
         connection.close()
 
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)    
+
 if __name__ == "__main__":
 	main()
